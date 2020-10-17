ad9e31ee0e00bd0ea4066cd4e4f7039b5bd406de
MikeyBerger <michaeljudahberger@gmail.com>
2020-10-16 18:25:14 -0700

    Game Added

---
+++ LightInTheDark/Assets/PlayerController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+
+    Vector2 PlayerMovement;
+    Vector2 LightMovement;
+    public float PlayerSpeed;
+    public float LightSpeed;
+    public bool IsJumping;
+    private Rigidbody2D RB;
+    private GameObject Light;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        RB = GetComponent<Rigidbody2D>();
+        Light = GameObject.FindGameObjectWithTag("Light").GetComponent<GameObject>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnPlayerMove(InputAction.CallbackContext ctx)
+    {
+        PlayerMovement = ctx.ReadValue<Vector2>();
+    }
+
+    public void OnLightMove(InputAction.CallbackContext ctx)
+    {
+        LightMovement = ctx.ReadValue<Vector2>();
+    }
+
+    public void OnJump(InputAction.CallbackContext ctx)
+    {
+        if (ctx.phase == InputActionPhase.Performed)
+        {
+            IsJumping = true;
+        }
+    }
+
+}
