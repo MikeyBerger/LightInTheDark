6b632d95bb3e9d2af9793461e33eee4d8d8a1923
MikeyBerger <michaeljudahberger@gmail.com>
2020-10-17 22:11:36 -0700

    Created Player
    Still need to fix some bugs

--- LightInTheDark/Assets/PlayerController.cs
+++ LightInTheDark/Assets/PlayerController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+//using UnityEngine.Animations;
 
 public class PlayerController : MonoBehaviour
 {
@@ -12,13 +13,15 @@ public class PlayerController : MonoBehaviour
     public float LightSpeed;
     public bool IsJumping;
     private Rigidbody2D RB;
-    private GameObject Light;
+    private Transform Light;
+    private Animator Anim;
 
     // Start is called before the first frame update
     void Start()
     {
         RB = GetComponent<Rigidbody2D>();
-        Light = GameObject.FindGameObjectWithTag("Light").GetComponent<GameObject>();
+        Light = GameObject.FindGameObjectWithTag("Light").GetComponent<Transform>();
+        Anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -27,6 +30,51 @@ public class PlayerController : MonoBehaviour
         
     }
 
+    private void FixedUpdate()
+    {
+        RB.velocity = new Vector2(PlayerMovement.x, 0) * PlayerSpeed * Time.deltaTime;
+
+        AnimatePlayer();
+        MoveLight();
+        Flip();
+    }
+
+    void AnimatePlayer()
+    {
+        if (PlayerMovement.x > 0 || PlayerMovement.x < 0)
+        {
+            Anim.SetBool("IsMoving", true);
+        }
+        else
+        {
+            Anim.SetBool("IsMoving", false);
+        }
+    }
+
+    void MoveLight()
+    {
+        Light.transform.Translate(new Vector3(LightMovement.x, LightMovement.y, 0) * LightSpeed * Time.deltaTime);
+    }
+
+    void Flip()
+    {
+        Vector3 Scale = transform.localScale;
+
+        if(PlayerMovement.x > 0)
+        {
+            Scale.x = 1;
+        }
+        else if (PlayerMovement.x < 0)
+        {
+            Scale.x = -1;
+        }
+
+        transform.localScale = Scale;
+    }
+
+
+
+    #region InputActions
     public void OnPlayerMove(InputAction.CallbackContext ctx)
     {
         PlayerMovement = ctx.ReadValue<Vector2>();
@@ -44,5 +92,6 @@ public class PlayerController : MonoBehaviour
             IsJumping = true;
         }
     }
+    #endregion
 
 }
